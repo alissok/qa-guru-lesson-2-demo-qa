958b5c37641066ef480260a29accfbe92cb66fa8
Alisa.Kadilenko <alisa.kadilenko@nexign.com>
2022-04-01 21:10:04 +0300

    change simpletest

--- src/test/java/guru/qa/SimpleTest.java
+++ src/test/java/guru/qa/SimpleTest.java
@@ -20,25 +20,19 @@ public class SimpleTest {
     @Test
     void assertTest(){
         System.out.println("###     @Test1");
-        Selenide.open("https://ya.ru");
         //... поиск в новостях
-        WebDriverRunner.closeWindow();
     }
 
     @Test
     void assertTest2() {
         System.out.println("###     @Test2");
-        Selenide.open("https://ya.ru");
         //... поиск в картинках
-        WebDriverRunner.closeWindow();
     }
 
     @Test
     void assertTest3() {
         System.out.println("###     @Test3");
-        Selenide.open("https://ya.ru");
         //... поиск в тексте
-        WebDriverRunner.closeWindow();
     }
 
     @AfterEach
@@ -50,6 +44,5 @@ public class SimpleTest {
     @AfterAll
     static void close() {
         System.out.println("###     @AfterAll");
-        WebDriverRunner.closeWindow();
     }
 }
