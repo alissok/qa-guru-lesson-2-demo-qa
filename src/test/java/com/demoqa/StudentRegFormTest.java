e675319167edcada87edc8be488e6da0ef5ee0c7
Alisa.Kadilenko <alisa.kadilenko@nexign.com>
2022-04-03 21:35:49 +0300

    New test

--- src/test/java/com/demoqa/StudentRegFormTest.java
+++ src/test/java/com/demoqa/StudentRegFormTest.java
@@ -1,62 +1,61 @@
 package com.demoqa;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.*;
 import org.apache.commons.lang3.RandomStringUtils;
-
-import java.util.GregorianCalendar;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byId;
-import static com.codeborne.selenide.Selenide.$$;
-
 import static com.codeborne.selenide.Selectors.byText;
-
-
 import static com.codeborne.selenide.Selenide.*;
 
 public class StudentRegFormTest {
+
+    //Personal Info
+    RandomStringUtils gen =  new RandomStringUtils();
+    String firstName = gen.randomAlphabetic(6);
+    String lastName = gen.randomAlphabetic(10);
+    String userEmail = gen.randomAlphabetic(7) + "@gmail.com";
+    String mobile = gen.randomNumeric(10);
+    String currAddress = gen.randomAlphabetic(15) + " " + gen.randomNumeric(3);
+    //States and Cities
+    SelenideElement stateAndCitySelector = $("#stateCity-wrapper");
+    String state = "Haryana";
+    String city = "Karnal";
+    //Gender
+    SelenideElement genterSelector = $("#genterWrapper");
+    String gender = "Male";
+    //Date
+    SelenideElement dateOfBirthSelector = $("#dateOfBirth-wrapper");
+    String month = "June";
+    String year = "2006";
+    String day = "013";
+    //Hobbies, Subject, Picture
+    String filename = "reallife.jpg";
+    String hobby = "Sports";
+    String subject  = "Art";
+    SelenideElement hobbiesSelector = $("#hobbiesWrapper");
+    //Text after registration
+    String thanks = "Thanks for submitting the form";
 
     @BeforeAll
     static void setUp() {
         Configuration.holdBrowserOpen = true;
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        open("https://demoqa.com");
     }
 
-    @AfterAll
-    static void shutDown() {
-//        closeWindow();
-//        closeWebDriver();
+    @BeforeEach
+    void createScrollablePage() {
+        Selenide.executeJavaScript(
+                "document.querySelector(\"footer\").hidden = 'true';" +
+                        "document.querySelector(\"#fixedban\").hidden = 'true'");
     }
-
-    //Personal Info
-    String firstName = RandomStringUtils.randomAlphabetic(6);
-    String lastName = RandomStringUtils.randomAlphabetic(10);
-    String userEmail = RandomStringUtils.randomAlphabetic(7) + "@gmail.com";
-    String mobile = RandomStringUtils.randomNumeric(10);
-    String currAddress = RandomStringUtils.randomAlphabetic(15) + " " + RandomStringUtils.randomNumeric(3);
-    //States and Cities
-    String subjects = "Alex Egorov";
-    SelenideElement stateAndCitySelector = $("#stateCity-wrapper");
-    String state = "Haryana";
-    String city = "Karnal";
-    //Hobbies
-    SelenideElement hobbiesSelector = $("#hobbiesWrapper");
-    //Gender
-    SelenideElement genterSelector = $("#genterWrapper");
-    String gender = "Male";
-    //Date
-    SelenideElement dateOfBirthSelector = $("#dateOfBirth-wrapper");
-    int month = 11;
-    String year = "2006";
-    String day = "013";
-    String filename = "reallife.jpg";
-    String subject  = "Art";
 
     @Test
-    void fillRegform() {
+    void fillRegForm() {
 
         open("/automation-practice-form");
 
@@ -70,14 +69,36 @@ public class StudentRegFormTest {
         $(".react-datepicker__year-select").selectOption(year);
         $(".react-datepicker__day--"+day).click();
         $("#subjectsInput").setValue(subject).pressEnter();
-        $(hobbiesSelector).$(byText("Sports")).click();
+        $(hobbiesSelector).$(byText(hobby)).click();
         $("#uploadPicture").uploadFromClasspath(filename);
         $("#currentAddress").setValue(currAddress);
+        $(byText("State and City")).scrollTo();
         $(stateAndCitySelector).$(byText("Select State")).click();
         $(stateAndCitySelector).$(byText(state)).click();
         $(stateAndCitySelector).$(byText("Select City")).click();
         $(stateAndCitySelector).$(byText(city)).click();
-        $("#submit").scrollTo();
-        $("#submit").click();
+        $("#submit").pressEnter();
+
+        //Asserts
+        $("#example-modal-sizes-title-lg").shouldHave(text(thanks));
+        $(".table-responsive").shouldHave(
+                text(firstName),
+                text(lastName),
+                text(userEmail),
+                text(gender),
+                text(mobile),
+                text(month + "," + year),
+                text(subject),
+                text(filename),
+                text(currAddress),
+                text(state + " " + city)
+        );
+        $("#closeLargeModal").click();
+        $(".practice-form-wrapper").shouldNotHave(//Check if form cached personal data
+                text(firstName),
+                text(lastName),
+                text(userEmail),
+                text(currAddress)
+        );
     }
 }
